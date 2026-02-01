2337c1486d766edf7478494d170f424aa085becc
Julius Boateng <juliusboateng@Juliuss-MacBook-Pro-2.local>
2026-02-01 15:06:51 -0600

    Rename project

--- trivia/models.py
+++ trivia/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+# 15X15 grid.
+# Free form / criss-cross puzzle because limited interlocking of words
+# https://puzzlemaker.discoveryeducation.com/criss-cross/result - generates puzzles
+# I don't need to build something to create the puzzle.
+# Just infrastructure to make it interactive and good puzzles themselves.
+# numpy array
+
+# Each question and answer would be stored. Make it re-usable. Can't really delete without affecting all other grids.
+# Probably would need to just create new grids with new word, then delete old word.
+
+# Id, Question, Answer
+
+
+# DB store, IDs and indexes to link words.
+
+
+# https://puzzlemaker.discoveryeducation.com/criss-cross/result
+
+
+
+# Cross word with word bank
+
