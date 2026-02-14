c68024519769f13f61b9e455afed9cb010af6af9
Julius Boateng <juliusopokuboateng@gmail.com>
2026-02-13 18:38:34 -0600

    Update models

--- criss_cross/models.py
+++ criss_cross/models.py
@@ -9,14 +9,21 @@ class Board(models.Model):
     class Meta:
         constraints = [
             models.CheckConstraint(
-                condition=models.Q(rows__gte=1) & models.Q(rows__lte=21),
+                condition=models.Q(rows__gte=5) & models.Q(rows__lte=21),
                 name="board_rows_range"),
             models.CheckConstraint(
-                condition=models.Q(cols__gte=1) & models.Q(cols__lte=21),
+                condition=models.Q(cols__gte=5) & models.Q(cols__lte=21),
                 name="board_cols_range"
+            ),
+            models.CheckConstraint(
+                condition=models.Q(cols=models.F("rows")),
+                name="board_row_col_symmetry"
             )
         ]
 
+'''
+Physical squares on the board
+'''
 class BoardCell(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     row_index = models.PositiveIntegerField()
@@ -32,17 +39,27 @@ class BoardCell(models.Model):
         ]
 
     def clean(self):
-        if self.row_index >= self.board.rows:
-            raise ValidationError("Row Index out of bounds")
-
         if self.col_index >= self.board.cols:
             raise ValidationError("Col Index out of bounds")
 
+        if self.row_index >= self.board.rows:
+            raise ValidationError("Row Index out of bounds")
+
+'''
+Questions/Answers for puzzle
+'''
 class Clue(models.Model):
     question = models.CharField(max_length=150)
     answer = models.CharField(max_length=21)
     length = models.PositiveIntegerField() # derived
 
+    def save(self, *args, **kwargs):
+        self.length = len(self.answer)
+        super().save(*args, **kwargs)
+
+'''
+Mapping between board and clue
+'''
 class CluePlacement(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
     clue = models.ForeignKey(Clue, on_delete=models.CASCADE)
@@ -61,23 +78,50 @@ class CluePlacement(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=["board", "start_row", "start_col", "direction"],
-                name="clue_placement_unique_row_col_direction")
+                name="clue_placement_unique_row_col_direction"),
+            models.UniqueConstraint(
+                fields=["board", "clue"],
+                name="clue_placement_unique_board_clue"
+            )
         ]
+    
+    def clean(self):
+        if self.start_col >= self.board.cols:
+            raise ValidationError("start_col out of bounds")
 
-# Derived from CluePlacement
+        if self.start_row >= self.board.rows:
+            raise ValidationError("start_row out of bounds")
+
+        if self.direction == "A":
+            if self.start_col + self.clue.length > self.board.cols:
+                raise ValidationError("Across clue overflows board")
+
+        if self.direction == "D":
+            if self.start_row + self.clue.length > self.board.rows:
+                raise ValidationError("Down clue overflows board")
+
+
+'''
+Mapping of answer to physical cells on the board
+'''
 class ClueCell(models.Model):
     clue_placement = models.ForeignKey(CluePlacement, on_delete=models.CASCADE)
     board_cell = models.ForeignKey(BoardCell, on_delete=models.CASCADE)
-    clue_index = models.PositiveIntegerField()
+    answer_index = models.PositiveIntegerField()
 
+    # It's fine for placements to appear on multiple cells.
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["clue_placement", "clue_index"],
-                name="unique_index_per_placement"
-            ),
-            models.UniqueConstraint(
                 fields=["clue_placement", "board_cell"],
-                name="unique_cell_per_placement"
-            )
+                name="clue_cell_unique_cell_per_placement"
+            ), # placements must only have one index for a specific cell
+            models.UniqueConstraint(
+                fields=["clue_placement", "answer_index"],
+                name="clue_cell_unique_index_per_placement"
+            ), # prevents the same index to mapping to different cells on the board
         ]
+
+    def clean(self):
+        if self.board_cell.board_id != self.clue_placement.board_id:
+            raise ValidationError("Cell must belong to same board")
